f12a2e8fd71975b9aefa4e38a68cf8ec11d6d5c6
Santiago <santiagowhite99@hotmail.com>
2020-05-13 20:51:14 -0500

    Grafica releases

--- src/app/releases/releases.component.ts
+++ src/app/releases/releases.component.ts
@@ -13,58 +13,64 @@ export class ReleasesComponent implements OnInit {
 
   releases: Release[];
   releaseSizes: number[];
+  releaseDates: string[];
   dataReady: boolean;
+  lineChartData: ChartDataSets[];
+  public lineChartLabels: Label[];
+
+
   constructor(private releasesService: ReleasesService) { }
 
   async ngOnInit(): Promise<void> {
-    // this.releaseSizes = [];
-    // this.dataReady = false;
-    // this.releasesService.getReleasesGit().subscribe(data => {
-    //   console.log(data[0].assets_url);
-
-    //   this.releases = data;
-    //   this.releases.forEach(element => {
-    //     let date = new Date(element.published_at);
-    //     element.published_at = date;
-    //     this.releasesService.getReleaseAssets(element.assets_url).subscribe(assets=>{
-    //       element.assets = assets;
-    //       this.releaseSizes.push(element.assets[0].size);
-    //       console.log(this.releaseSizes);
-    //       this.lineChartData = [{ data: this.releaseSizes, label: 'Series A' }];
-    //     });
-    //     this.releasesService.getReleasesGit().toPromise()
-
-    //   });
-    //   this.dataReady = true;
-    //   console.log(this.releases[0].assets[0].size);
-    // });
+  
 
     this.releaseSizes = [];
+    this.releaseDates = [];
     this.dataReady = false;
+    // console.log(localStorage.getItem('releasesizes'));
+    // console.log(localStorage.getItem('releaseDates'));
+    let releasesizesTemp = JSON.parse('['+localStorage.getItem('releasesizes')+']');
+    let releaseDatesTemp = localStorage.getItem('releaseDates').split(',');
+
+   
 
     let data = await this.releasesService.getReleasesGit().toPromise();
 
-      this.releases = data;
-      this.releases.forEach(async element => {
-        let date = new Date(element.published_at);
-        element.published_at = date;
-        let assets = await this.releasesService.getReleaseAssets(element.assets_url).toPromise();
-        console.log(assets);
-        element.assets = assets;
-        if(assets.length>0){
-          this.releaseSizes.push(assets[0].size);
-        }
-      });
-      this.dataReady = true;
+    this.releases = data;
+    for(var i = 0; i < this.releases.length;i++) {
+      let element = this.releases[i];
+      let date = new Date(element.published_at);
+      element.published_at = date;
+      let assets = await this.releasesService.getReleaseAssets(element.assets_url).toPromise();
+      element.assets = assets;
+      if (assets.length > 0) {
+        this.releaseSizes.push(assets[0].size/1000000);
+        this.releaseDates.push(element.published_at.toLocaleDateString());
+      }
+    }
+    this.lineChartData = [
+      { data: this.releaseSizes.reverse(), label: 'Tamaño Release (MB)' }
+    ];
+    this.lineChartLabels = this.releaseDates.reverse();
+    // console.log(this.releaseSizes.toString());
+    // localStorage.setItem('releasesizes',this.releaseSizes.toString());
+    // localStorage.setItem('releaseDates',this.releaseDates.toString());
+    // console.log(localStorage.getItem('releasesizes'));
+    // console.log(JSON.parse('['+localStorage.getItem('releasesizes')+']'));
+    this.dataReady = true;
+
+    
+
+
   }
 
   //Chart Info
-  public lineChartData: ChartDataSets[] = [
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series A' },
-    // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    // { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C', yAxisID: 'y-axis-1' }
-  ];
-  public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  // public lineChartData: ChartDataSets[] = [
+  //   { data: [28, 48, 40, 19, 86, 27, 90], label: 'Tamaño Release' },
+  //   // { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+  //   // { data: [180, 480, 770, 90, 1000, 270, 400], label: 'Series C', yAxisID: 'y-axis-1' }
+  // ];
+  // public lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Aug'];
 
   public lineChartOptions: (ChartOptions & { annotation: any }) = {
     responsive: true,
@@ -83,27 +89,13 @@ export class ReleasesComponent implements OnInit {
             color: 'rgba(255,0,0,0.3)',
           },
           ticks: {
-            fontColor: 'red',
+            fontColor: 'grey',
           }
         }
       ]
     },
     annotation: {
-      annotations: [
-        {
-          type: 'line',
-          mode: 'vertical',
-          scaleID: 'x-axis-0',
-          value: 'March',
-          borderColor: 'orange',
-          borderWidth: 2,
-          label: {
-            enabled: true,
-            fontColor: 'orange',
-            content: 'LineAnno'
-          }
-        },
-      ],
+      annotations: [],
     },
   };
   public lineChartColors: Color[] = [
@@ -115,22 +107,6 @@ export class ReleasesComponent implements OnInit {
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgba(148,159,177,0.8)'
     },
-    { // dark grey
-      backgroundColor: 'rgba(77,83,96,0.2)',
-      borderColor: 'rgba(77,83,96,1)',
-      pointBackgroundColor: 'rgba(77,83,96,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(77,83,96,1)'
-    },
-    { // red
-      backgroundColor: 'rgba(255,0,0,0.3)',
-      borderColor: 'red',
-      pointBackgroundColor: 'rgba(148,159,177,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-    }
   ];
   public lineChartLegend = true;
   public lineChartType = 'line';

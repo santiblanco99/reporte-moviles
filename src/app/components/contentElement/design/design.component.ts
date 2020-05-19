cccc5095b53c1c23e9b1919d8fd3f5a28e2d8c2a
Sergio Yepes <sa.yepes@uniandes.edu.co>
2020-05-18 21:17:57 -0500

    design component modified

--- src/app/components/contentElement/design/design.component.ts
+++ src/app/components/contentElement/design/design.component.ts
@@ -16,18 +16,172 @@ export class DesignComponent implements OnChanges {
     url: '',
     name: 'Fonts\'s photo'
   }
+
+  natriumFontNunitoSansExamplePhoto: CustomImage = {
+    url: '',
+    name: 'Nunito Sans font example'
+  }
+
+  natriumFontNunutoSansExampleCode: string = `
+  // Text style for alert dialog header
+  static TextStyle textStyleDialogHeader(BuildContext context) {
+    return TextStyle(
+      fontFamily: "NunitoSans",
+      fontSize: AppFontSizes._large,
+      fontWeight: FontWeight.w700,
+      color: StateContainer.of(context).curTheme.primary,
+    );
+  }
+
+  // Text style for dialog options
+  static TextStyle textStyleDialogOptions(BuildContext context) {
+    return TextStyle(
+      fontFamily: "NunitoSans",
+      fontSize: AppFontSizes.medium,
+      fontWeight: FontWeight.w600,
+      color: StateContainer.of(context).curTheme.text,
+    );
+  }
+  `;
+
+  themePhotosList: CustomImage[] = [
+    {
+      name: 'Natrium',
+      url: ''
+    },
+    {
+      name: 'Titanium',
+      url: ''
+    },
+    {
+      name: 'Indium',
+      url: ''
+    },
+    {
+      name: 'Neptunium',
+      url: ''
+    },
+    {
+      name: 'Thorium',
+      url: ''
+    }
+  ]
+  changingThemeGif: CustomImage = {
+    name: '',
+    url: ''
+  }
+
+  changeThemeCode: string = `
+  Future<void> _themeDialog() async {
+    ThemeOptions selection = await showAppDialog<ThemeOptions>(
+        context: context,
+        builder: (BuildContext context) {
+          return AppSimpleDialog(
+            title: Padding(
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: Text(
+                AppLocalization.of(context).themeHeader,
+                style: AppStyles.textStyleDialogHeader(context),
+              ),
+            ),
+            children: _buildThemeOptions(),
+          );
+        });
+    if (_curThemeSetting != ThemeSetting(selection)) {
+      sl
+          .get<SharedPrefsUtil>()
+          .setTheme(ThemeSetting(selection))
+          .then((result) {
+        setState(() {
+          StateContainer.of(context).updateTheme(ThemeSetting(selection));
+          _curThemeSetting = ThemeSetting(selection);
+        });
+      });
+    }
+  }
+  `;
+
+
+  languageDe: CustomImage = {
+    name: '',
+    url: ''
+  }
+
+  languageAr: CustomImage = {
+    name: '',
+    url: ''
+  }
+
+  changeLanguageCode : string = `
+  Future<void> _languageDialog() async {
+    AvailableLanguage selection = await showAppDialog<AvailableLanguage>(
+        context: context,
+        builder: (BuildContext context) {
+          return AppSimpleDialog(
+            title: Padding(
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: Text(
+                AppLocalization.of(context).language,
+                style: AppStyles.textStyleDialogHeader(context),
+              ),
+            ),
+            children: _buildLanguageOptions(),
+          );
+        });
+    sl
+        .get<SharedPrefsUtil>()
+        .setLanguage(LanguageSetting(selection))
+        .then((result) {
+      if (StateContainer.of(context).curLanguage.language != selection) {
+        setState(() {
+          StateContainer.of(context).updateLanguage(LanguageSetting(selection));
+        });
+      }
+    });
+  }
+  `;
+
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges) {
     if (this.commonImages && this.commonImages.length > 0) {
-      const themePhotoTemp = this.commonImages.find(item => item.name.includes('natrium_themes'));
-      const natriumFontsPhotoTemp = this.commonImages.find(item => item.name.includes('natrium_fonts'));
-      if (themePhotoTemp) {
-        this.themePhoto = { ...themePhotoTemp };
+      for (let item of this.commonImages) {
+        if (item.name.includes('theme_natrium')) {
+          this.themePhotosList[0].url = item.url;
+        }
+        else if (item.name.includes('theme_titanium')) {
+          this.themePhotosList[1].url = item.url;
+        }
+        else if (item.name.includes('theme_indium')) {
+          this.themePhotosList[2].url = item.url;
+        }
+        else if (item.name.includes('theme_neptunium')) {
+          this.themePhotosList[3].url = item.url;
+        }
+        else if (item.name.includes('theme_thorium')) {
+          this.themePhotosList[4].url = item.url;
+        }
+        else if (item.name.includes('natrium_fonts')) {
+          this.natriumFontsPhoto = { ...item };
+        }
+        else if (item.name.includes('changing_theme_vid')) {
+          this.changingThemeGif = { ...item };
+        }
+        else if (item.name.includes('font_nunito_sans_example')) {
+          this.natriumFontNunitoSansExamplePhoto = { ...item };
+        }
+        else if (item.name.includes('language_de')) {
+          this.languageDe = { ...item };
+        }
+        else if (item.name.includes('language_ar')) {
+          this.languageAr = { ...item };
+        }
       }
-      if (natriumFontsPhotoTemp){
-        this.natriumFontsPhoto = {...natriumFontsPhotoTemp};
-      }
+      // const themePhotosListTemp = this.commonImages.(item => item.name.includes('theme_'));
+      // const natriumFontsPhotoTemp = this.commonImages.find(item => item.name.includes('natrium_fonts'));
+      // if (natriumFontsPhotoTemp) {
+      //   this.natriumFontsPhoto = { ...natriumFontsPhotoTemp };
+      // }
     }
   }
 }
